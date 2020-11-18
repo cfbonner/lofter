94e00b14150e7f3d59a971e520113aa45e9971e9
cfbonner <cfbonner@gmail.com>
2020-11-18 13:55:09 +0000

    Add home controller and template

---
+++ lib/lofter_web/controllers/home_controller.ex
@@ -0,0 +1,7 @@
+defmodule LofterWeb.HomeController do
+  use LofterWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+end
--- lib/lofter_web/router.ex
+++ lib/lofter_web/router.ex
@@ -17,7 +17,8 @@ defmodule LofterWeb.Router do
   scope "/", LofterWeb do
     pipe_through :browser
 
-    live "/", PageLive, :index
+    get "/", HomeController, :index
+    # live "/", PageLive, :index
   end
 
   # Other scopes may use custom stacks.
