436a244904de1633bd589fb3a261ffb220a62cc8
cfbonner <cfbonner@gmail.com>
2020-12-30 00:30:05 +0000

    Add js scroll polyfill

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
+
+import "smoothscroll-polyfill"
 import "fontsource-source-sans-pro"
 import "../css/app.scss"
 import live_sockect from "./live_socket"
