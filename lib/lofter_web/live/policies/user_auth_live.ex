d0f56d7dd2d540e7551fd514bb92e40706e7306a
cfbonner <cfbonner@gmail.com>
2022-02-11 12:22:03 +0000

    Update phoenix

--- lib/lofter_web/live/policies/user_auth_live.ex
+++ lib/lofter_web/live/policies/user_auth_live.ex
@@ -1,7 +1,7 @@
 defmodule LofterWeb.UserLiveAuth do
   import Phoenix.LiveView
 
-  def mount(_params, %{"user_token" => token} = _session, socket) do
+  def on_mount(:default, _params, %{"user_token" => token}, socket) do
     if current_user = Lofter.Accounts.get_user_by_session_token(token) do
       socket =
         assign_new(socket, :current_user, fn ->
