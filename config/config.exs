1734e5a9adb076efa5b33adbb7a7115c7c9c150d
cfbonner <cfbonner@gmail.com>
2022-08-19 10:49:48 +0100

    Update esbuild target
    - es2017 minimum for running alpine js

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,7 @@ config :lofter, LofterWeb.Endpoint,
 config :esbuild,
   version: "0.12.26",
   default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+    args: ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
